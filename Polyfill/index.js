98e37586be5c8824d180028fe8f280d887a6d0c7
Sandeep Shah <sandeep2016shah@gmail.com>
2025-05-25 13:10:02 +0600

    promises polyfill

--- Polyfill/index.js
+++ Polyfill/index.js
@@ -1,17 +1,71 @@
+//Resolve, if all the promises gets resolved else rejected
 Promise.all = (promises) => {
+  return new Promise((resolve, reject) => {
+    const result = new Array(promises.length);
+    let count = 0;
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then((value) => {
+          result[index] = value;
+          count++;
+          if (count === promises.length) {
+            resolve(result);
+          }
+        })
+        .catch((error) => reject(error));
+    });
+  });
+};
+
+// resolve all the promises, either resolved or rejected
+Promise.allSettled = (promises) => {
+  return new Promise((resolve) => {
+    const result = new Array(promises.length);
+    let count = 0;
+    promises.forEach((p, index) => {
+      Promise.resolve(p)
+        .then((value) => {
+          result[index] = { status: "fulfilled", value };
+        })
+        .catch((reason) => {
+          result[index] = { status: "rejected", reason };
+        })
+        .finally(() => {
+          count++;
+          if (count === promises.length) resolve(result);
+        });
+    });
+  });
+};
+
+// resolved first fullfilled promise or reject with AggregrateError if all rejected
+Promise.any = (promises) => {
+  return new Promise((resolve, reject) => {
+    const rejectCount = 0;
+    const errors = [];
+    promises.forEach((p) => {
+      Promise.resolve(p)
+        .then((value) => resolve(value))
+        .catch((error) => {
+          errors[rejectCount] = error;
+          rejectCount++;
+        })
+        .finally(() => {
+          if (rejectCount === promises.length) {
+            reject(new AggregateError(errors, "All promises are rejected"));
+          }
+        });
+    });
+  });
+};
+
+// resolve or reject once first promise gets settles
+Promise.race = (promises) => {
     return new Promise((resolve, reject) => {
-        const result = new Array(promises.length);
-        let count = 0;
-        promises.forEach((p, index) => {
+        promises.forEach((p) => {
             Promise.resolve(p)
-                .then((value) => {
-                    result[index] = value;
-                    count++;
-                    if(count === promises.length){
-                        resolve(result);
-                    }
-                })
-                .catch((error) => reject(error));
+                .then((value) => resolve(value))
+                .catch((reason) => reject(reason))
         })
     })
 }
