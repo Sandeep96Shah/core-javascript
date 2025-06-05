094c11a0cab3f0c1830d774c68b52449a0c125b7
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-05 10:33:03 +0600

    Sign Up

---
+++ Basics/index.js
@@ -0,0 +1,7 @@
+const users = [ 
+    { id: 1, name: 'Alice', role: 'Admin' }, 
+    { id: 2, name: 'Bob', role: 'Editor' }, 
+    { id: 3, name: 'Charlie', role: 'Viewer' } 
+   ]; 
+   
+   console.table(users); 
