be18ef1f4bfdbe8b24ac9f9d71fb5802133d5dfe
Sandeep Shah <sandeep2016shah@gmail.com>
2025-07-03 12:08:34 +0530

    palindrome string

--- StringQuestions/easy.js
+++ StringQuestions/easy.js
@@ -18,4 +18,24 @@ function reverseStrReduce(str){
 
 console.log(reverseStrReduce(str));
 
+// Check for a palindrome
+
+const str1 = "madam";
+const str2 = "hello";
+
+function palindromeStr(str){
+    let end = str.length - 1;
+    let start = 0;
+    while(start <= end){
+        if(str[start] !== str[end]){
+            return "No, its not a palindrome";
+        }
+        start++;
+        end--;
+    }
+    return "Yes, its a palindrome";
+}
+console.log(palindromeStr(str1));
+console.log(palindromeStr(str2));
+
 
