9ce653c4f45d0925cfb897d83b374cb8031a06f2
Sandeep Shah <sandeep2016shah@gmail.com>
2025-07-05 20:38:30 +0530

    compress string algorithm

--- StringQuestions/medium.js
+++ StringQuestions/medium.js
@@ -31,3 +31,26 @@ function firstNonRepeatingChar1(str) {
 console.log(firstNonRepeatingChar1(repeatingStr1));
 console.log(firstNonRepeatingChar1(repeatingStr2));
 console.log(firstNonRepeatingChar1(nonRepeatingStr));
+
+// 2. Implement a basic string compression algorithm
+const str = "aabcccccaaa";
+
+function compressStr(str) {
+  const n = str.length;
+  let result = "";
+  let i = 0;
+  while (i < str.length) {
+    let count = 1;
+    let start = i + 1;
+    const ch = str[i];
+    while (start < str.length && ch === str[start]) {
+      count++;
+      start++;
+    }
+    i = start;
+    result += ch;
+    result += count;
+  }
+  return result;
+}
+console.log(compressStr(str));
