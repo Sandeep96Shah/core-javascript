aca1db665f9040a18efcb6fa4845a17ba55ceb37
Sandeep Shah <sandeep2016shah@gmail.com>
2025-07-06 22:45:29 +0530

    Move all zeros to the end of the array

--- ArrayQuestions/index.js
+++ ArrayQuestions/index.js
@@ -23,3 +23,24 @@ function getSecondLargestNum(nums) {
 }
 
 console.log(getSecondLargestNum([1, 1, 1, 1, 1, 1, 1]));
+
+// Move all zeros to the end of the array
+function moveAllZerosAtEnd(nums) {
+  let nonZeroIndex = 0;
+
+  const n = nums.length;
+
+  for (const num of nums) {
+    if (num) {
+      nums[nonZeroIndex++] = num;
+    }
+  }
+
+  while (nonZeroIndex < n) {
+    nums[nonZeroIndex++] = 0;
+  }
+
+  return nums;
+}
+
+console.log(moveAllZerosAtEnd([0, 1, 0, 3, 12]));
