d9a3d1d9fc74515359aa333136afbf2fbe2a68fa
Sandeep Shah <sandeep2016shah@gmail.com>
2025-07-06 22:27:29 +0530

    find second largest number from array

--- ArrayQuestions/index.js
+++ ArrayQuestions/index.js
@@ -1,7 +1,25 @@
 // 1. Remove duplicates from a sorted array
-function removeDuplicates(nums){
-    return [...new Set(nums)];
+function removeDuplicates(nums) {
+  return [...new Set(nums)];
 }
 
 console.log(removeDuplicates([1, 1, 2, 3, 3]));
 
+// 2. Find the second largest number in an array
+
+function getSecondLargestNum(nums) {
+  let firstLargest = -1;
+  let secondLargest = -2;
+  if (!nums.length) return -1;
+  for (const num of nums) {
+    if (num > firstLargest) {
+      secondLargest = firstLargest;
+      firstLargest = num;
+    } else if (num > secondLargest && num !== firstLargest) {
+      secondLargest = num;
+    }
+  }
+  return secondLargest;
+}
+
+console.log(getSecondLargestNum([1, 1, 1, 1, 1, 1, 1]));
